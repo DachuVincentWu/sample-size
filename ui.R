dd4020e11f67941c602539d7eafb1848acef6d85
DachuVincentWu <b01801001@ntu.edu.tw>
2014-12-05 00:22:53 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,43 +5,46 @@ library(htmltools)
 shinyUI(fluidPage(
   
   titlePanel("Sample Size Calculator"),
-
-  sidebarLayout(
-    sidebarPanel(
-      #helpText("Please choose your study design. "),
-      
-      selectInput("data_type",label=h5("Data Type"),
-                  choices = list("---Please Select---"=0,
-                                 "Binary"=1,"Quantitative"=2,"Survival"=3,"Ordinal"=4)),
+  #fluidRow(column(3, offset = 8,tags$h4(textOutput("sam_bin")))),
+  sidebarLayout(fluid = T,
+    sidebarPanel(width = 5,
+      radioButtons("data_type",label=HTML("<font color=003D79><big>Data Type</big></font>"),selected = 0,
+                   choices = list("Binary"=1,"Quantitative"=2,"Survival"=3,"Ordinal"=4)),
+                 
+      #selectInput("data_type",label=h5("Data Type"),,width = '400px',
+      #           choices = list("---Please Select---"=0,"Binary"=1,"Quantitative"=2,"Survival"=3,"Ordinal"=4)),
       uiOutput("design"),
-      br(),br(),br(),br(),
-      br(),br(),br(),br(),br(),
+      tags$hr(),
+      HTML("<font color=003D79><big>Result</big></font>"),
+      br(),br(),
+      uiOutput("sam_bin"),
+      br(),tags$hr(),
       HTML("<font color=8E8E8E><small><i>Da-Chu Wu,2014</i></small></font>")
     ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
+    mainPanel(width = 6,
       #tags$style(type="text/css",
-       #          ".shiny-output-error { visibility: hidden; }",
-        #         ".shiny-output-error:before { visibility: hidden; }"),
+      #          ".shiny-output-error { visibility: hidden; }",
+      #         ".shiny-output-error:before { visibility: hidden; }"),
       
-      numericInput("alpha", 
-                   label = HTML("<b><big>Type I error(&alpha;)</big></b>"),
-                   value = 0.05,step=0.01,max=1,min=0),
-      numericInput("power",label = HTML("<b><big>Power(1-&beta;)</big></b>"),
-                   value = 0.9,step=0.05,max=1,min=0),
-      br(),
-      uiOutput("mode1"),
-      uiOutput("mode2"),
-      br(),
-      uiOutput("mode_var"),
-      tags$hr(),
-      tags$h4(textOutput("sam")),
-      tags$hr(),
-      br(),
-      uiOutput("img_prop_def"),
-      br(),
-      br()
-    )
+      #numericInput("alpha", 
+       #            label = HTML("<b><big>Type I error(&alpha;)</big></b>"),
+        #           value = 0.05,step=0.01,max=1,min=0),
+      tabsetPanel(
+      tabPanel("Parameters",
+        uiOutput("mode1"),br(),
+        uiOutput("mode_var"),
+        tags$hr(),
+        #uiOutput("run"),
+        br(),br()
+            ),
+      tabPanel("Information",
+        uiOutput("img_prop_def")
+        
+               )
+      )
+    )    
   )
+  
 ))
+
