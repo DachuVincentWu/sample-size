dfc6cc2f7e03b06683304da68326a073097ec7d5
DachuVincentWu <b01801001@ntu.edu.tw>
2014-12-05 03:42:18 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,42 +5,36 @@ library(htmltools)
 shinyUI(fluidPage(
   
   titlePanel("Sample Size Calculator"),
-  #fluidRow(column(3, offset = 8,tags$h4(textOutput("sam_bin")))),
+  
   sidebarLayout(fluid = T,
     sidebarPanel(width = 5,
       radioButtons("data_type",label=HTML("<font color=003D79><big>Data Type</big></font>"),selected = 0,
                    choices = list("Binary"=1,"Quantitative"=2,"Survival"=3,"Ordinal"=4)),
                  
-      #selectInput("data_type",label=h5("Data Type"),,width = '400px',
-      #           choices = list("---Please Select---"=0,"Binary"=1,"Quantitative"=2,"Survival"=3,"Ordinal"=4)),
+      #selectInput("data_type",label=h5("Data Type"),,width = '400px',choices = list("---Please Select---"=0,"Binary"=1,"Quantitative"=2,"Survival"=3,"Ordinal"=4)),
       uiOutput("design"),
       tags$hr(),
       HTML("<font color=003D79><big>Result</big></font>"),
       br(),br(),
       uiOutput("sam_bin"),
-      br(),tags$hr(),
-      HTML("<font color=8E8E8E><small><i>Da-Chu Wu,2014</i></small></font>")
+      br(),tags$hr()
+  
     ),
     
     mainPanel(width = 6,
       #tags$style(type="text/css",
       #          ".shiny-output-error { visibility: hidden; }",
       #         ".shiny-output-error:before { visibility: hidden; }"),
-      
-      #numericInput("alpha", 
-       #            label = HTML("<b><big>Type I error(&alpha;)</big></b>"),
-        #           value = 0.05,step=0.01,max=1,min=0),
       tabsetPanel(
       tabPanel("Parameters",
-        uiOutput("mode1"),br(),
+        uiOutput("mode_bin"),br(),
         uiOutput("mode_var"),
-        tags$hr(),
+        tags$hr()
         #uiOutput("run"),
-        br(),br()
             ),
-      tabPanel("Information",
-        uiOutput("img_prop_def")
-        
+      tabPanel("Manual",
+        uiOutput("img_prop_def"),br(),
+        tags$hr()
                )
       )
     )    
